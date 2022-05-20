bca145b7f42f2f26faf398a9454ff3a9d7621383
Parshva-Jogani <87229239+Parshva-Jogani@users.noreply.github.com>
2022-05-20 14:27:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDTlj8owL5Wov59yLs7_7Sw34JVt4EubTg",
+  authDomain: "storytelling-2091b.firebaseapp.com",
+  databaseURL: "https://storytelling-2091b-default-rtdb.firebaseio.com",
+  projectId: "storytelling-2091b",
+  storageBucket: "storytelling-2091b.appspot.com",
+  messagingSenderId: "514050498519",
+  appId: "1:514050498519:web:2f8b3cbd3abafab9b3f592"
+};
